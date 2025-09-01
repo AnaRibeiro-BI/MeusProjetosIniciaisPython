410e9f8a11e769d3067f6035e82921bb7da638c8
Ana Luyza Ribeiro Barros <analuyza.9@hotmail.com>
2025-09-01 23:48:13 +0000

    aula 9 - funções pytho parte 1

--- aula9.py
+++ aula9.py
@@ -15,19 +15,68 @@
 
 # --------------------------------------------- Exercício 1
 # Calculadora de Média - Criar uma função media(n1, n2)
-def media(n1, n2, n3, n4):
-    return (n1 + n2 + n3 + n4) / 4
+# def media(n1, n2, n3, n4):
+#     return (n1 + n2 + n3 + n4) / 4
 
-nomealuno = input("Qual o nome do aluno: ")
+# nomealuno = input("Qual o nome do aluno: ")
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    n1=float(input("Digite a nota da N1: "))
-    n2=float(input("Digite a nota da n2: "))
-    n3=float(input("Digite a nota da n3: "))
-    n4=float(input("Digite a nota da n4: "))
+#     n1=float(input("Digite a nota da N1: "))
+#     n2=float(input("Digite a nota da n2: "))
+#     n3=float(input("Digite a nota da n3: "))
+#     n4=float(input("Digite a nota da n4: "))
 
-    r=media(n1, n2, n3, n4)
-    print(f"A média do(a) aluno(a) {nomealuno} foi de {r:.2f} e suas notas foram  {n1}, {n2}, {n3} e {n4}")
+#     r=media(n1, n2, n3, n4)
+#     print(f"A média do(a) aluno(a) {nomealuno} foi de {r:.2f} e suas notas foram  {n1}, {n2}, {n3} e {n4}")
 
 # -------------------------------------------- Exercício 2
+# Mensagem Personalizada - Crie uma função mensagem_personalizada(nome, curso) que imprima "Olá, [nome]" Bem vindo ao curso de [curso]."
+# def mensagem_personalizada(nome, curso):
+#     return 
+# print("Oi, {nome}! Seja bem vindo ao curso de {curso}")
+
+# nome = input("Digite o nome do aluno: ")
+# curso = input("Digite o curso do aluno: ")
+
+# if __name__ == '__main__': 
+#     print(f"Oi, {nome}! Seja bem vindo ao curso de {curso}!")
+
+# -------------------------------------------- Exercício 3
+# Criando funções úteis
+
+    # cálculo de área de retângulo (base*altura)
+# def area_retangulo(b,h):
+#     return (b * h)
+# b = float(input("Digite a base do retângulo: "))
+# h = float(input("Digite a altura do retângulo: "))
+# r = b * h
+
+# if __name__ == '__main__': 
+#     print(f"A área do seu retângulo é de {r}")
+
+    # função para converter Celsius para Fahrenheit
+# def conversor_temp(celsius):
+#     return (celsius * 9/5) + 32
+
+# celsius = float(input("Digite a temperatura em Celsius: "))
+# r = (celsius * 9/5) + 32
+
+# if __name__ == '__main__': 
+#     print(f"A temperatura em Fahreheit é de {r}.")
+
+    # função de saudação baseada no horário
+def saudacao_horario(nome, hora):
+    if 5<= hora < 12: 
+        return (f"Boa tarde, {nome}!")
+    elif 12<= hora < 18:
+        return (f"Boa noite, {nome}!")
+    else: 
+        return ("Horário inválido.")
+
+nome = input("Digite o seu nome: ")
+hora = int(input("Digite apenas a hora: "))
+msg = ({saudacao_horario}, {nome})
+
+if __name__ == '__main__': 
+    print(f"{msg}")
