618e350a087124431a141a5a741ef96a5450d65b
Ana Luyza Ribeiro Barros <analuyza.9@hotmail.com>
2025-10-13 22:51:23 +0000

    atualizar

--- Aula 20 - Projeto de Gerenciamento/menu.py
+++ Aula 20 - Projeto de Gerenciamento/menu.py
@@ -1,2 +1,13 @@
 def menu():
-    p
+    print("Menu - Gerenciamento do Restaurante")
+    print("1. Cadastrar Produto")
+    print("2. Cadastrar Valor do Produto")
+    print("3. Listar Produtos")
+    print("4. Remover Produto")
+    print("5. Realizar Pedido")
+    print("6. Listar Itens do Pedido")
+    print("7. Cadastrar Atendente")
+    print("8. Listar Atendentes")
+    print("9. Sair")
+    escolha = input("Escolha uma opção: ")
+    return escolha
