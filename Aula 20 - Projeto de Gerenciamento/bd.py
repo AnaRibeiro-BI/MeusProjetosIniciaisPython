4a7c78884b0c42ee111b38b8d2d1989d02f99212
Ana Luyza Ribeiro Barros <analuyza.9@hotmail.com>
2025-10-13 23:22:36 +0000

    atualização do banco de dados e funções para o projeto de gerenciamento de restaurante.

--- Aula 20 - Projeto de Gerenciamento/bd.py
+++ Aula 20 - Projeto de Gerenciamento/bd.py
@@ -9,17 +9,18 @@ agora = datetime.datetime.now()
 
 cursor = conexao.cursor()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS produtos (
+try:
+    cursor.execute("""CREATE TABLE IF NOT EXISTS produtos (
                id INTEGER PRIMARY KEY AUTOINCREMENT, 
                nome TEXT, valor REAL)""")
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS itens (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS itens (
                id_item INTEGER PRIMARY KEY AUTOINCREMENT,
                id_produto INTEGER, 
                quantidade INTEGER,
                valor_unitario REAL)""")
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS pedidos (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS pedidos (
                id_pedido INTEGER PRIMARY KEY AUTOINCREMENT, 
                data_hora_abertura TIMESTAMP,
                data_hora_fechamento TIMESTAMP,
@@ -28,9 +29,12 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS pedidos (
                id_atendente INTEGER,
                valor_total REAL)""")
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS atendentes (
+    cursor.execute("""CREATE TABLE IF NOT EXISTS atendentes (
                id INTEGER PRIMARY KEY AUTOINCREMENT,
                nome TEXT)""") 
+
+except sqlite3.Error as erro:
+    print(f"Erro no banco: {erro}")
                
 conexao.commit()
 conexao.close()
--- Aula 20 - Projeto de Gerenciamento/functions.py
+++ Aula 20 - Projeto de Gerenciamento/functions.py
@@ -1,10 +1,16 @@
 class Produto:
-    def __init__(self, id, nome, valor):
+     def __init__(self, id, nome, valor):
         self.id = id
         self.nome = nome
         self.valor = valor
 
-nome = input("Digite o nome do produto: ")
-valor = float(input("Digite o valor do produto: ")) 
+# nome = input("Digite o nome do produto: ")
+# valor = float(input("Digite o valor do produto: ")) 
 
-class Item: 
+def cadastrar_produto(id, nome):
+    from bd import conexao # Importa a conexão do banco de dados
+    cursor = conexao.cursor() # Cria um cursor para executar comandos SQL
+    cursor.execute("INSERT INTO produtos (id, nome) VALUES (?, ?)", (id, nome)) # Insere o produto e seu valor na tabela
+    conexao.commit() # Salva as alterações no banco de dados
+    print(f"Produto '{nome}' cadastrado com sucesso!")
+    return True
