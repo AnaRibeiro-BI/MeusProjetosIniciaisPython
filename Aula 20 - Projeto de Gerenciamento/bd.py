f740f796b3bb65cee6780c7699306bdc0e35ea4d
Ana Luyza Ribeiro Barros <analuyza.9@hotmail.com>
2025-10-13 22:35:01 +0000

    novo projeto aula 20

---
+++ Aula 20 - Projeto de Gerenciamento/bd.py
@@ -0,0 +1,38 @@
+import sqlite3
+import datetime
+
+#cria o banco de dados para o restautante
+conexao = sqlite3.connect("restaurante.db")
+
+# Obtém a data e hora atuais
+agora = datetime.datetime.now()
+
+cursor = conexao.cursor()
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS produtos (
+               id INTEGER PRIMARY KEY AUTOINCREMENT, 
+               nome TEXT, valor REAL)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS itens (
+               id_item INTEGER PRIMARY KEY AUTOINCREMENT,
+               id_produto INTEGER, 
+               quantidade INTEGER,
+               valor_unitario REAL)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS pedidos (
+               id_pedido INTEGER PRIMARY KEY AUTOINCREMENT, 
+               data_hora_abertura TIMESTAMP,
+               data_hora_fechamento TIMESTAMP,
+               status TEXT,
+               id_item INTEGER,
+               id_atendente INTEGER,
+               valor_total REAL)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS atendentes (
+               id INTEGER PRIMARY KEY AUTOINCREMENT,
+               nome TEXT)""") 
+               
+conexao.commit()
+conexao.close()
+
+               
---
+++ Aula 20 - Projeto de Gerenciamento/functions.py
@@ -0,0 +1,10 @@
+class Produto:
+    def __init__(self, id, nome, valor):
+        self.id = id
+        self.nome = nome
+        self.valor = valor
+
+nome = input("Digite o nome do produto: ")
+valor = float(input("Digite o valor do produto: ")) 
+
+class Item: 
